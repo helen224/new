d512cb4851ff55924624d482f6970f5a57ddc2cd
helen224 <1292501663@qq.com>
2016-07-09 16:43:19 +0800

    Create atm.rb

---
+++ features/step_definitions/atm.rb
@@ -0,0 +1,31 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+require 'cucumber'
+require 'atm_iml.rb'
+ 
+Given(/^the user's account has (\d+)$/) do |balance|
+  @balance = balance
+end
+
+When(/^the user asks the ATM for (\d+)$/) do |cash|
+  @cash = cash
+end
+
+Then(/^the user's account will have (\d+)$/) do |new_balance|
+  @atm=Atm.new
+  @new_balance=@atm.get_newbalance(@balance,@cash)
+  if @new_balance==new_balance
+	@status = 'success'
+  else 
+    @status = 'fail'
+  end
+end
+Then(/^the withdrawal result will be success$/) do
+ @status.should=='success'
+end
+
+Then(/^the withdrawal result will be fail$/) do
+  @status.should=='fail'
+end
+
+
+
